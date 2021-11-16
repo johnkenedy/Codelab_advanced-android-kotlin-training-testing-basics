0c9f475e032086b89b3598259129fa18659e1c57
JohnKenedy <john.kenedy0410@gmail.com>
2021-11-16 17:13:51 -0300

    ServiceLocator

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
@@ -38,21 +38,22 @@ class DefaultTasksRepository(
     private val tasksLocalDataSource: TasksDataSource,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO) : TasksRepository {
 
-    companion object {
-        @Volatile
-        private var INSTANCE: DefaultTasksRepository? = null
-
-        fun getRepository(app: Application): DefaultTasksRepository {
-            return INSTANCE ?: synchronized(this) {
-                val database = Room.databaseBuilder(app,
-                ToDoDatabase::class.java, "Tasks.db")
-                    .build()
-                DefaultTasksRepository(TasksRemoteDataSource, TasksLocalDataSource(database.taskDao())).also {
-                    INSTANCE = it
-                }
-            }
-        }
-    }
+    //Created a ServiceLocator.provideTaskRepository, so we don't need this
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: DefaultTasksRepository? = null
+//
+//        fun getRepository(app: Application): DefaultTasksRepository {
+//            return INSTANCE ?: synchronized(this) {
+//                val database = Room.databaseBuilder(app,
+//                ToDoDatabase::class.java, "Tasks.db")
+//                    .build()
+//                DefaultTasksRepository(TasksRemoteDataSource, TasksLocalDataSource(database.taskDao())).also {
+//                    INSTANCE = it
+//                }
+//            }
+//        }
+//    }
 
 //    init {
 //        val database = Room.databaseBuilder(application.applicationContext,
